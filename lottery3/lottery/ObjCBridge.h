b89a3f2ea43b1a4d330b5903d32ac47dc2c345e0
alan guo <g594xz@gmail.com>
2016-05-28 14:13:57 +1200

    add file

---
+++ lottery3/lottery/ObjCBridge.h
@@ -0,0 +1,13 @@
+//
+//  ObjCBridge.h
+//  lottery
+//
+//  Created by Alan on 16/5/27.
+//  Copyright © 2016年 lottery. All rights reserved.
+//
+#import <EstimoteSDK/EstimoteSDK.h>
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+
+#endif /* ObjCBridge_h */
